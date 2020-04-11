fb314b27a3a0a1a71066354368f1b3a91d0f6961
abhishek.jha-at-864372041473 <abhishek.jha@quantiphi.com>
2020-04-11 21:12:43 +0530

    socket url issue fix for heroku

--- public/client/src/app/components/home/home.module.ts
+++ public/client/src/app/components/home/home.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { ApiInterceptor } from '../../interceptors/app.interceptor';
 
+import { environment } from '../../../environments/environment';
 import { ApiService } from '../../api.service';
 import { HomeService } from './home.service';
 import { MaterialModule } from '../../material.module';
@@ -11,7 +12,7 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { DialogComponent } from '../../shared/dialog/dialog.component';
 
-const config: SocketIoConfig = { url: 'http://localhost:5000', options: { path: '/api/socket/doc' } };
+const config: SocketIoConfig = { url: environment.baseUrl, options: { path: '/api/socket/doc' } };
 
 @NgModule({
   declarations: [HomeComponent, DialogComponent],
